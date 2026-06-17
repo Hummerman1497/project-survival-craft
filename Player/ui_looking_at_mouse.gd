89ab6c7a596ff1908a274b853f460ab6cbd8a684
Hummerman1497 <janisgroeger@gmail.com>
2026-06-17 15:12:08 +0200

    Mouse direction indicator INNIT

---
+++ Player/ui_looking_at_mouse.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+var mouse_position : Vector2 = Vector2.ZERO
+
+func _process(_delta: float) -> void:
+	# Schritt 1: Vektor vom Spieler zur Maus berechnen und normalisieren
+	mouse_position = get_global_mouse_position()
+	var direction_to_mouse: Vector2 = (mouse_position - global_position).normalized()
+	
+	# Schritt 2: Winkel im Bogenmaß (Radiant) holen
+	var angle: float = direction_to_mouse.angle()
+	
+	# Den eingerasteten Winkel zurück in einen sauberen Richtungs-Vektor umwandeln
+	rotation = angle + PI
+	
+	
+	
