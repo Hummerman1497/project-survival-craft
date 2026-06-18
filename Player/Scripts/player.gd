a1f33eac46d5a9994755245683dacd9f1b0a8cfe
Hummerman1497 <janisgroeger@gmail.com>
2026-06-18 11:33:04 +0200

    player direction umgeschrieben

--- Player/Scripts/player.gd
+++ Player/Scripts/player.gd
@@ -17,11 +17,11 @@ func _ready():
 	
 
 func _process(delta):		
-	direction.x = Input.get_action_strength("right") - Input.get_action_strength("left") # wenn beide taste gedrückt werden kein movement
-	direction.y = Input.get_action_strength("down") - Input.get_action_strength("up") # wenn beide taste gedrückt werden kein movement
-
-	if direction.length() > 1.0:
-		direction = direction.normalized()			
+	direction = Vector2(
+		Input.get_axis("left","right"),
+		Input.get_axis("up","down")
+	).normalized()
+			
 	
 	
 func _physics_process(delta):
